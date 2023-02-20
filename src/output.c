ebd27bc2f7dba6168ba954ae265ea9f0a5c3a631
Nate Choe <nate@natechoe.dev>
2023-02-20 17:12:54 -0600

    Licensing

--- src/output.c
+++ src/output.c
@@ -1,3 +1,19 @@
+// ttf2psf - A simple, working utility to generate Linux console fonts
+// Copyright (C) 2023  Nate Choe
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILIY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -74,8 +90,8 @@ next_equivalence:
 			/* This goto is basically a 2 level continue statement,
 			 * you can't do that in C all too well. */
 		}
-		fputs("Equivalence file includes characters not in the "
-				"charset!\n", stderr);
+		fputs("Warning: Equivalence file includes characters not in "
+				"the charset!\n", stderr);
 	}
 	/* Generate charset */
 
